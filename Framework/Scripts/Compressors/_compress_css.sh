9cdd16f2ad74c8a6297f111743f313e40dddb2e2
fatherbrennan <bjt.lepore@gmail.com>
2021-11-27 15:53:48 +1030

    update compressor tag

--- Framework/Scripts/Compressors/_compress_css.sh
+++ Framework/Scripts/Compressors/_compress_css.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # 
 # @fatherbrennan
-(echo -e '/**\n * \n * \n * @fatherbrennan\n */'; cat $1 | sed 's@/\*.*\*/@@g;/\/\*/,/\*\//d' | tr -d '\n' | tr -s ' ' ' ' | sed 's/ {/{/g;s/{ /{/g;s/} /}/g;s/ }/}/g;s/: /:/g;s/ :/:/g;s/; /;/g;s/ ;/;/g;s/, /,/g;s/ ,/,/g') > $2
+(echo -e '/**\n * \n * \n * @fatherbrennan@CSScompressor\n */'; cat $1 | sed 's@/\*.*\*/@@g;/\/\*/,/\*\//d;s/[[:blank:]]*/ /' | tr -d '\n' | tr -s ' ' ' ' | sed 's/ {/{/g;s/{ /{/g;s/} /}/g;s/ }/}/g;s/: /:/g;s/ :/:/g;s/; /;/g;s/ ;/;/g;s/, /,/g;s/ ,/,/g;s/^ *//') > $2

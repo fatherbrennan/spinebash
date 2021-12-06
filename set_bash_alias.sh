9f1d0b5bf712255755cb69876ee2bacabc560c06
fatherbrennan <bjt.lepore@gmail.com>
2021-12-07 08:32:06 +1030

    add executable permissions in bash alias script

--- set_bash_alias.sh
+++ set_bash_alias.sh
@@ -6,5 +6,6 @@ a=".bashrc"
 b=$(grep 'alias spine=' ~/"$a" | wc -c)
 [ "$b" -gt 0 ] || echo 'alias spine="./spine"'>>~/"$a"
 source ~/"$a"
+chmod 755 spine Framework/Scripts/* Framework/Scripts/Tools/* Framework/Scripts/Compressors/*
 rm -rf set_bash_alias.sh
 exit 0

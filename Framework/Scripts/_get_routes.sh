73bd66a08e6651c6db8cba79f66a5240cf303b1e
fatherbrennan <bjt.lepore@gmail.com>
2022-01-08 11:00:03 +1030

    complete update

--- Framework/Scripts/_get_routes.sh
+++ Framework/Scripts/_get_routes.sh
@@ -2,40 +2,56 @@
 # 
 # @fatherbrennan
 
-# Get relative path
-
 # Resources
-routes='Resources/routes.sh'
+ROUTES='Resources/routes.sh'
 
+# Imports
+source 'Framework/Scripts/Tools/ansi_esc_codes.sh'
+
+# Track all router arguments 
+router_args=''
+
+#
+# {$1} invalid route declaration error message. preferred format ': <<error_message>>'
+# {$2} line number
+# ?{$3} error
+#
 throw_exception()
 {
-    echo -e "line ${1}: Error: Invalid route declaration${3}\n \e[31m${2}\e[39m\nUsage: ROUTE 'new/url/path' VIEW 'path/to/file'" && exit 1
+    [ -z "$1" ] && local error_message='' || local error_message="Error: Invalid route declaration${1}\n"
+    [ -z "$2" ] && local line='' || local line="line ${2}: " 
+    [ -z "$3" ] && local error='' || local error="${COLOR_RED}${3}${TEXT_RESET}\n"
+    local declaration="Declare Route: ROUTE 'url/path' VIEW 'path/to/file'"
+    echo -e "${line}${error_message} ${error}${declaration}" && exit 1
 }
 
+#
+# {$1} line number
+# {$2} line contents
+#
 read_routes()
 {
     # Turn input into array of strings
     IFS=' ' read -r -a args <<< "$2"
 
     # Throw exception for too many arguments declared
-    [ "${#args[@]}" -gt "4" ] && throw_exception "$1" "$2" ': <<Too many arguments>>'
+    [ "${#args[@]}" -gt "4" ] && throw_exception ': <<Too many arguments or unexpected space>>' "$1" "$2"
 
     # Prossess only lines of correct arguments
     if [ "${#args[@]}" -eq "4" ]
     then
         # Throw invalid declaration
-        [ "ROUTE" = "${args[0]}" ] && [ "VIEW" = "${args[2]}" ] || throw_exception "$1" "$2"
-        # VIEW is valid file path
+        [ "ROUTE" = "${args[0]}" ] && [ "VIEW" = "${args[2]}" ] || throw_exception ': <<Invalid Syntax>>' "$1" "$2"
+        # Check if VIEW is a valid file path
         f=$(echo "${args[3]}" | sed "s/'//g")
-        [ -f "Resources/views/content/${f}.html" ] || throw_exception "$1" "$2" ': <<Missing file>>'
-        # Is a valid url route
-        [[ "$f" =~ ^[A-Za-z][-A-Za-z0-9_:./]*$ ]] || throw_exception "$1" "$2" ': <<Invalid URL>>'
+        [ -f "Resources/views/content/${f}.html" ] || throw_exception ': <<Missing file>>' "$1" "$2"
+        router_args+="$f\n"
+        # Check if ROUTE is a valid url path
+        r=$(echo "${args[1]}" | sed "s/'//g")
+        [[ "$r" =~ [^-[:alnum:]\/\.\_\~\#\+\?\*\&\%\!\=] ]] && throw_exception ': <<Invalid URL>>' "$1" "$2" && true
+        router_args+="$r\n"
     fi
 }
-
-# Views are unique
-duplicates=$(echo $(cat "$routes") | grep -Po "(?<=(VIEW ')).*(?=')" | uniq -d)
-[ -n "$duplicates" ] && echo -e "Routes: Error: Invalid route declaration: <<Duplicate view path>>\n\e[31m${duplicates}\e[39m\n->VIEW 'path/to/file' must be unique" && exit 1
 
 # Read the routes file by line
 line=0
@@ -43,5 +59,9 @@ while IFS= read -r l || [[ -n "$l" ]]
 do
     ((line++))
     # Dont read comments (hash lines)
-    [[ "$l" == \#* ]] || read_routes "$line" "$l"
-done < "$routes"
+    [[ "$l" =~ ^[[:space:]]*?\# ]] || read_routes "$line" "$l"
+done < "$ROUTES"
+
+# Check if routes and views are unique
+duplicates=$(echo -e "$router_args" | sort | uniq -d)
+[ -n "$duplicates" ] && throw_exception ': <<Duplicate>>' '' "$duplicates" || echo -n

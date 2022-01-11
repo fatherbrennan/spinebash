cca0b09abc17b7e07036f2a7fa918dbb218251dd
fatherbrennan <bjt.lepore@gmail.com>
2022-01-11 19:46:07 +1030

    minor tweaks

--- Framework/Scripts/dev.sh
+++ Framework/Scripts/dev.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
-# 
+#
 # @fatherbrennan
 
-res1=$(date +%s.%N)
+# Start runtime timer
+t1=$(date +%s.%N)
+
+source 'Framework/.env'
+source "$TOOLS_ANSI_ESC_CODES"
 
 # Get relative path from arg
-app=$1
+app="$1"
 
 # Track asset files
 assets=""
 
-# Create cache if it doesn't exist
+# Create cache directory if it doesn't exist
 [ -d "${app}Framework/Scripts/cache/tmp" ] || mkdir -p "${app}Framework/Scripts/cache/tmp"
 
 # Run build scripts
@@ -22,25 +26,24 @@ assets=""
 # Print
 print_box()
 {
-    local a=("$@")
-    local b=$(tput cols)-2
-    p(){ echo -ne "${2}\e[2m${1}\e[0m"; }
-    p "\u250c";for ((i=0;i<$b;i++));do p "\u2500";done;p "\u2510"
-    for i in "${a[@]}";do local d=$(($b-${#i}-1));p "\u2502";printf %"$d"s; p "\u2502" "\e[32m${i}\e[0m ";done
-    p "\u2514";for ((i=0;i<$b;i++));do p "\u2500";done;p "\u2518"
+    local a=("$@");local b=$(tput cols)-3;local c='\u2500';local d='\u2502';printf "$TEXT_DIM"
+    p(){ printf "$1"; };p_line(){ for ((i=0;i<=$b;i++));do p "$c";done }
+    p '\u250c';p_line;p '\u2510'
+    for i in "${a[@]}";do local e=$(($b-${#i}));p "$d";printf "%${e}s";p "${TEXT_RESET}${COLOR_GREEN}${i}${TEXT_RESET}${TEXT_DIM} ${d}";done
+    p '\u2514';p_line;p '\u2518'
 }
 
 # Loop through all asset files
 for f in $(find "${app}Public/" -type f)
 do
     # Track assets relative path
-    assets+="$(echo "$f" | sed "s#$app##")\n"
+    assets+="${f//$app} "
 done
 
 # Print assets
 if [ ! -z "$assets" ]
 then
-    res2=$(date +%s.%N)
-    echo -ne "\n\e[32m  ASSETS BUILD TIME: ";echo -e $res2 $res1 's\e[0m' | awk '{print $1 - $2 $3}'
-    print_box $(echo -e "$(echo $assets | sed 's/\(.*\)\\n/\1/')")
+    t2=$(date +%s.%N)
+    printf "\n${COLOR_GREEN}%s%fs${TEXT_RESET}\n" '  ASSETS BUILD TIME: ' "$(printf "${t1} ${t2}" | awk '{print $2 - $1}')"
+    print_box $(printf "$assets")
 fi

4f829aaad7a94ff57c26ec87f76c66716898af9a
fatherbrennan <bjt.lepore@gmail.com>
2022-01-12 15:52:58 +1030

    minor tweaks

--- Framework/Scripts/dev.sh
+++ Framework/Scripts/dev.sh
@@ -12,7 +12,7 @@ source "$TOOLS_ANSI_ESC_CODES"
 app="$1"
 
 # Track asset files
-assets=""
+assets=()
 
 # Create cache directory if it doesn't exist
 [ -d "${app}Framework/Scripts/cache/tmp" ] || mkdir -p "${app}Framework/Scripts/cache/tmp"
@@ -23,13 +23,15 @@ assets=""
 "${app}Framework/Scripts/_make_js.sh" || exit 1
 "${app}Framework/Scripts/_make_markup.sh" || exit 1
 
-# Print
+#
+# {$1} string to print [ expandable string ]
+#
 print_box()
 {
-    local a=("$@");local b=$(tput cols)-3;local c='\u2500';local d='\u2502';printf "$TEXT_DIM"
+    local a=("$@");local b=$(tput cols)-3;local c='\u2500';local d='\u2502';printf "$TEXT_DIM";local l="${#a[@]}"
     p(){ printf "$1"; };p_line(){ for ((i=0;i<=$b;i++));do p "$c";done }
     p '\u250c';p_line;p '\u2510'
-    for i in "${a[@]}";do local e=$(($b-${#i}));p "$d";printf "%${e}s";p "${TEXT_RESET}${COLOR_GREEN}${i}${TEXT_RESET}${TEXT_DIM} ${d}";done
+    for ((i=0;i<"$l";i++));do local e=$(($b-${#a[i]}));p "$d";printf "%${e}s";p "${TEXT_RESET}${COLOR_GREEN}${a[i]}${TEXT_RESET}${TEXT_DIM} ${d}";done
     p '\u2514';p_line;p '\u2518'
 }
 
@@ -37,7 +39,7 @@ print_box()
 for f in $(find "${app}Public/" -type f)
 do
     # Track assets relative path
-    assets+="${f//$app} "
+    assets+=("${f//$app}")
 done
 
 # Print assets
@@ -45,5 +47,5 @@ if [ ! -z "$assets" ]
 then
     t2=$(date +%s.%N)
     printf "\n${COLOR_GREEN}%s%fs${TEXT_RESET}\n" '  ASSETS BUILD TIME: ' "$(printf "${t1} ${t2}" | awk '{print $2 - $1}')"
-    print_box $(printf "$assets")
+    print_box "${assets[@]}"
 fi

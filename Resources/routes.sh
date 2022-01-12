64222e35476760852a311f5d0b93930304aec57e
fatherbrennan <bjt.lepore@gmail.com>
2022-01-12 21:06:37 +1030

    minor tweaks

--- Resources/routes.sh
+++ Resources/routes.sh
@@ -2,17 +2,18 @@
 # Spinebash
 # Declare app routes in this file
 ################################################################
-# 
+#
 # ROUTE
 #  ->Unique
 #  ->URL Hash route
-# 
+#
 # VIEW
 #  ->Unique
 #  ->File name (no extension: assumed html)
 #  ->Relative path from: 'Resources/views/content'
-# 
-# 
+#
+#
 
 ROUTE 'home/' VIEW 'index'
 ROUTE 'newpage/' VIEW 'new_page_dir/new_page_file'
+# ROUTE 'newpage/' VIEW 'new_page_dir/new_page_file'

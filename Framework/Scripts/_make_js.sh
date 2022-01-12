42f6d7e3001eab91b77d4e8a28b68925fb62c195
fatherbrennan <bjt.lepore@gmail.com>
2022-01-12 19:27:34 +1030

    add .env parent dependency

--- Framework/Scripts/_make_js.sh
+++ Framework/Scripts/_make_js.sh
@@ -3,28 +3,21 @@
 # @fatherbrennan
 
 # Imports
-source '.config'
-source 'Framework/Scripts/Tools/is_true.sh'
-source 'Framework/Scripts/Tools/set_file_path.sh'
-
-# Asset directory
-a='Public/js/'
-
-# Resources
-b='Resources/js/'
-
-# Controller
-router='Framework/Controllers/localRouterController.js'
+source 'Framework/.env'
+source "$CONFIG"
+source "$TOOLS_GET_TMP"
+source "$TOOLS_IS_TRUE"
+source "$TOOLS_PRINT_L"
+source "$TOOLS_SET_FILE_PATH"
 
 # Limit temp files to the framework cache
-tmp1="Framework/Scripts/cache/tmp/.tmp${RANDOM}"
-scripts='Framework/Scripts/cache/tmp/scripts'
+tmp1=$(get_tmp)
 
 # Clear directory
-rm -rf "$a"*
+rm -rf "$PUBLIC_DIR_JS"*
 
-# Prepare temp script
-set_file_path "$scripts"
+# Prepare script cache file
+set_file_path "$CACHE_SCRIPTS"
 
 # Check config file if scripts should use defer attribute
 is_true "$JS_DEFER" && defer=' defer' || defer=''
@@ -32,10 +25,12 @@ is_true "$JS_DEFER" && defer=' defer' || defer=''
 # Track if router needs to be added
 add_router=true
 
-# Add html script tag to cache
+#
+# {$1} file path
+#
 add_html_script_tag()
 {
-    echo "<script src=\"js/$(basename $1)\"${defer}></script>">>"$scripts"
+    (print_l "<script src=\"js/$(basename $1)\"${defer}></script>")>>"$CACHE_SCRIPTS"
 }
 
 # Add asset files from resources
@@ -44,49 +39,48 @@ then
     # Use framework compressor if falsy value
     if [ -z "$USE_COMPRESSOR_JS" ]
     then
-        compressor='Framework/Scripts/Compressors/_compress_js.sh'
-        for f in $(find "$b" -type f -name '*.js')
+        compressor="$COMPRESSORS_JS"
+        for f in $(find "$RESOURCES_DIR_JS" -type f -name '*.js')
         do
             # Asset path
-            script="${a}${f##*/}"
+            script="${PUBLIC_DIR_JS}${f##*/}"
             # Track populated files
-            [ -s "$f" ] && echo "$f"
+            [ -s "$f" ] && print_l "$f"
             # Add router to first asset
-            is_true "$add_router" && router_contents=$(cat "$router") && add_router=false || router_contents=''
+            is_true "$add_router" && router_contents=$(cat "$CONTROLLERS_LOCAL_JS") && add_router=false || router_contents=''
             # Check config file and compress asset file if true, then add assets
-            (echo -e "$router_contents"; cat "$f")>"$tmp1"
+            (echo -e "$router_contents";cat "$f")>"$tmp1"
             $compressor "$tmp1" "$script"
             # Add script tag to temp file
             add_html_script_tag "$script"
         done
     else
         # Asset file name
-        mini_js="app.min.js"
         input_files=''
-        for f in $(find "$b" -type f -name '*.js')
+        for f in $(find "$RESOURCES_DIR_JS" -type f -name '*.js')
         do
             # Track populated files
-            [ -s "$f" ] && echo "$f"
+            [ -s "$f" ] && print_l "$f"
             # Add router
             input_files+="${f} "
         done
         # Add script tag to temp file
-        add_html_script_tag "$mini_js"
+        add_html_script_tag "$OUTPUT_COMPRESSED_JS"
         compressor=${USE_COMPRESSOR_JS//INPUT/$input_files}
-        compressor=${compressor//OUTPUT/$a$mini_js}
+        compressor=${compressor//OUTPUT/$PUBLIC_DIR_JS$OUTPUT_COMPRESSED_JS}
         # Unsafe execute
         eval $compressor
     fi
-    echo "COMPRESSOR (JavaScript): ${compressor}"
+    print_l "COMPRESSOR (JavaScript): ${compressor}"
 else
-    for f in $(find "$b" -type f -name '*.js')
+    for f in $(find "$RESOURCES_DIR_JS" -type f -name '*.js')
     do
         # Asset path
-        script="${a}${f##*/}"
+        script="${PUBLIC_DIR_JS}${f##*/}"
         # Track populated files
-        [ -s "$f" ] && echo "$f"
+        [ -s "$f" ] && print_l "$f"
         # Add router to first asset
-        is_true "$add_router" && router_contents=$(cat "$router") && add_router=false || router_contents=''
+        is_true "$add_router" && router_contents=$(cat "$CONTROLLERS_LOCAL_JS") && add_router=false || router_contents=''
         # Check config file and compress asset file if true, then add assets
         (echo -e "$router_contents";cat "$f")>"$tmp1"
         (cat "$tmp1")>"$script"

4dd2b7b967ea4c00acd2cb32ba6bb959685aee21
fatherbrennan <bjt.lepore@gmail.com>
2022-01-12 15:52:30 +1030

    Revert "minor tweaks"
    This reverts commit 50e917de7c1557a1719836680b9be29ae5c0864b.

--- Framework/Scripts/dev.sh
+++ Framework/Scripts/dev.sh
@@ -5,7 +5,6 @@
 # Start runtime timer
 t1=$(date +%s.%N)
 
-# Imports
 source 'Framework/.env'
 source "$TOOLS_ANSI_ESC_CODES"
 
@@ -13,26 +12,24 @@ source "$TOOLS_ANSI_ESC_CODES"
 app="$1"
 
 # Track asset files
-assets=()
+assets=""
 
 # Create cache directory if it doesn't exist
 [ -d "${app}Framework/Scripts/cache/tmp" ] || mkdir -p "${app}Framework/Scripts/cache/tmp"
 
 # Run build scripts
-# "${app}Framework/Scripts/_get_routes.sh" || exit 1
-# "${app}Framework/Scripts/_make_css.sh" || exit 1
-# "${app}Framework/Scripts/_make_js.sh" || exit 1
-# "${app}Framework/Scripts/_make_markup.sh" || exit 1
+"${app}Framework/Scripts/_get_routes.sh" || exit 1
+"${app}Framework/Scripts/_make_css.sh" || exit 1
+"${app}Framework/Scripts/_make_js.sh" || exit 1
+"${app}Framework/Scripts/_make_markup.sh" || exit 1
 
-#
-# {$1} string to print [ expandable string ]
-#
+# Print
 print_box()
 {
-    local a=("$@");local b=$(tput cols)-3;local c='\u2500';local d='\u2502';printf "$TEXT_DIM";local l="${#a[@]}"
+    local a=("$@");local b=$(tput cols)-3;local c='\u2500';local d='\u2502';printf "$TEXT_DIM"
     p(){ printf "$1"; };p_line(){ for ((i=0;i<=$b;i++));do p "$c";done }
     p '\u250c';p_line;p '\u2510'
-    for ((i=0;i<"$l";i++));do local e=$(($b-${#a[i]}));p "$d";printf "%${e}s";p "${TEXT_RESET}${COLOR_GREEN}${a[i]}${TEXT_RESET}${TEXT_DIM} ${d}";done
+    for i in "${a[@]}";do local e=$(($b-${#i}));p "$d";printf "%${e}s";p "${TEXT_RESET}${COLOR_GREEN}${i}${TEXT_RESET}${TEXT_DIM} ${d}";done
     p '\u2514';p_line;p '\u2518'
 }
 
@@ -40,13 +37,13 @@ print_box()
 for f in $(find "${app}Public/" -type f)
 do
     # Track assets relative path
-    assets+=("${f//$app}")
+    assets+="${f//$app} "
 done
 
-# Print assets (if exists)
+# Print assets
 if [ ! -z "$assets" ]
 then
     t2=$(date +%s.%N)
     printf "\n${COLOR_GREEN}%s%fs${TEXT_RESET}\n" '  ASSETS BUILD TIME: ' "$(printf "${t1} ${t2}" | awk '{print $2 - $1}')"
-    print_box "${assets[@]}"
+    print_box $(printf "$assets")
 fi

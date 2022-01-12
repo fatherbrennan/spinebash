e389c218e56c1caf96ac47a98e6a8b11f869d47f
fatherbrennan <bjt.lepore@gmail.com>
2022-01-12 12:41:10 +1030

    initial

---
+++ Framework/Scripts/Tools/set_file_path.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#
+# @fatherbrennan
+
+#
+# {$1} file path
+#
+set_file_path()
+{
+    if [ ! -f "$1" ]
+    then
+        local a="${1##*/}"
+        if [[ "$1" =~ [/] ]]
+        then
+            local b="${1%/*}/"
+            mkdir -p "$b" && touch "${b}${a}" || exit 1
+        else
+            touch "$a" || exit 1
+        fi
+    else
+        truncate -s 0 "$1"
+    fi
+}

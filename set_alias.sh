fe3954687756c7174da593891696d8cb265237ae
fatherbrennan <bjt.lepore@gmail.com>
2022-01-13 18:36:16 +1030

    remove echo

--- set_alias.sh
+++ set_alias.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 #
 # @fatherbrennan
+
 a=".bashrc"
 b=".zshrc"
 add()
 {
     [ -f ~/"$1" ] || touch ~/"$1"
     d=$(grep 'alias spine=' ~/"$1" | wc -c)
-    [ "$d" -gt 0 ] || echo 'alias spine="./spine"'>>~/"$1"
+    [ "$d" -gt 0 ] || printf '%s' 'alias spine="./spine"'>>~/"$1"
     source ~/"$1"
 }
 add "$a"
 c=$(ps | grep zsh | wc -c)
-echo "$c"
 [ "$c" -gt 0  ] && add "$b"
 chmod 700 spine Framework/Scripts/* Framework/Scripts/Tools/* Framework/Scripts/Compressors/*
 rm -rf set_alias.sh

8fadbc68e3625b50abe0be48700185f9221edf96
fatherbrennan <bjt.lepore@gmail.com>
2022-01-13 18:32:36 +1030

    use printf over echo

--- Framework/Scripts/Compressors/_compress_css.sh
+++ Framework/Scripts/Compressors/_compress_css.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 #
 # @fatherbrennan
-(echo -en "/**\n * \n * \n * @fatherbrennan:compressor:css\n */\n"; cat $1 | tr -d '\n' | perl -pe 's`/\*(.*?)\*/``gs' | tr -s ' ' ' ' | perl -pe 's`[\s]?[{][\s]?`{`g;s`[\s]?[}][\s]?`}`g;s`[\s]?[:][\s]?`:`g;s`[\s]?[;][\s]?`;`g;s`[\s]?[,][\s]?`,`g;s`[\s]?[/][\s]?`/`g;s`[\s]?[(][\s]?`(`g;s`[\s]?[)][\s]?`)`g')>"$2"
+(printf "/**\n * \n * \n * @fatherbrennan:compressor:css\n */\n";cat "$1" | tr -d '\n' | perl -pe 's`/\*(.*?)\*/``gs' | tr -s ' ' ' ' | perl -pe 's`[\s]?[{][\s]?`{`g;s`[\s]?[}][\s]?`}`g;s`[\s]?[:][\s]?`:`g;s`[\s]?[;][\s]?`;`g;s`[\s]?[,][\s]?`,`g;s`[\s]?[/][\s]?`/`g;s`[\s]?[(][\s]?`(`g;s`[\s]?[)][\s]?`)`g')>"$2"
--- Framework/Scripts/Compressors/_compress_html.sh
+++ Framework/Scripts/Compressors/_compress_html.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 #
 # @fatherbrennan
-(echo -en "<!--\n - \n - \n - @fatherbrennan:compressor:html\n -->\n"; cat $1 | tr -d '\n' | tr -s ' ' ' ' | perl -pe 's%<!--(.*?)-->%%gs;s%[\s]?[>][\s]?[<][\s]?%><%g;s%[\s]?/>%>%g')>"$2"
+(printf "<!--\n - \n - \n - @fatherbrennan:compressor:html\n -->\n"; cat $1 | tr -d '\n' | tr -s ' ' ' ' | perl -pe 's%<!--(.*?)-->%%gs;s%[\s]?[>][\s]?[<][\s]?%><%g;s%[\s]?/>%>%g')>"$2"

b5a1bce2aa5017ae59a58f3fa84dc1c4ffc2485e
fatherbrennan <bjt.lepore@gmail.com>
2022-01-24 14:25:51 +1030

    remove all but src/ directory on clear

--- Framework/Scripts/clear.sh
+++ Framework/Scripts/clear.sh
@@ -5,5 +5,8 @@
 # Imports
 source 'Framework/.env'
 
-find "$PUBLIC_DIR" -type f -delete
+# Remove all directories and contents except src/
+for d in $(find "$PUBLIC_DIR"* -maxdepth 0 -type d ! -name src); do rm -rf "$d"; done
+# Remove top level files
+find "$PUBLIC_DIR"* -maxdepth 0 -type f -delete
 find "$CACHE_DIR" -type f -delete

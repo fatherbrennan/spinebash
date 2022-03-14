77a4e0acff6fbcdfc959da67c84b5b6f7ced97aa
fatherbrennan <bjt.lepore@gmail.com>
2022-03-14 13:18:13 +1030

    update assets

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,12 +1,25 @@
-function redirect(route){window.location.hash=`/${route}`;}// Navigate to the app home page defined in routes on startup
-window.addEventListener('DOMContentLoaded', function() {
-    if(window.location.hash === '' || !window.location.hash) {
-        redirect('home/');
-    }
+function redirect(route){window.location.hash=`/${route.replace(/^((#\/)|([#])|([\/])|)/,'').replace(/([\/]|)$/,'/')}`;}// Declare the app home route
+const routeAppHome = 'home/';
+
+// Navigate to the home page if no route is declared
+window.addEventListener('DOMContentLoaded', function redirectHome() {
+    if (window.location.hash === '' || !window.location.hash) redirect(routeAppHome);
+});
+
+// Listen to every hash navigation event and redirect to 404 page if invalid hash URL
+window.addEventListener('hashchange', function redirectHashHandler() {
+    // Treat 'index/' and 'index' as 'index/'
+    const a = window.location.hash.replace(/^#\//, '').replace(/([\/]|)$/, '/');
+    // Navigate to the home page if no route is declared
+    if (a === '/') redirect(routeAppHome);
+    // Navigate to 404 page if no valid route is declared
+    else if (document.getElementById(`/${a}`) === null) redirect('http_404/');
+    // Otherwise, navigate to declared route
+    else redirect(a);
 });
 
 // JavaScript navigation
 // -> Click the button on the new page to redirect back to the homepage
-document.getElementById('newpage-button').addEventListener('click',function(){
+document.getElementById('newpage-button').addEventListener('click', function () {
     redirect('home/');
-})
+});
